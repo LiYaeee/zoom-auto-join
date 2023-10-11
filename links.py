374bd4f926caad050b67c85d266a7d5c286a9d52
LiYaeee <138414374+LiYaeee@users.noreply.github.com>
2023-10-11 08:02:30 +0800

    Add files via upload

---
+++ links.py
@@ -0,0 +1,12 @@
+from subj import *
+
+#links
+FB = "https://www.facebook.com/"
+FIL = "https://us04web.zoom.us/j/2011103406?pwd=TGF0ZmhKUUpGYWp1SlFSUi9mdW5jQT09"
+CC104 = "https://us04web.zoom.us/j/4112729029?pwd=RzVFeGN6T0IrNVhKZUVYZ1JZbVNZdz09"
+DS101 = "https://us05web.zoom.us/j/7359337600?pwd=grvu5eafKKPb7GgCjXACr1BZ6E1lvi.1"
+PE = "https://us05web.zoom.us/j/6345728769?pwd=MyMrDbLbCsz2FkHqEaimE3VtadOiQC.1"
+SDF104 = "https://us04web.zoom.us/j/2571737788?pwd=MWxyeVBmZE9EN2MrWUdXelV6ZVR0dz09"
+IAS101 = "https://us05web.zoom.us/j/6762064088?pwd=M3VLcUhoYWJyWDVnbGNBUUphaVB2dz09"
+ACTG = "https://us05web.zoom.us/j/5166843887?pwd=vXgkltvWNXTxB04hN9mLemewVnaGwo.1"
+MST = "https://us04web.zoom.us/j/8037321928?pwd=N1h5UmRBVW00YVlPM3hsQkNwUW9rUT09"
